840038ce47315bf2b2c84306dc8a397a3267badd
hyeonm9 <hyeonm9@hyeonm9ui-MacBookAir.local>
2026-06-02 20:48:21 +0900

    🚀 Release: v1.0.5 - Auto-sync at 2026-06-02 20:48:21

--- test_spotify.swift
+++
@@ -1,7 +0,0 @@
-import AppKit
-
-let bundleId = "com.spotify.client"
-let runningApps = NSWorkspace.shared.runningApplications
-let isRunning = runningApps.contains { $0.bundleIdentifier == bundleId }
-
-print("Is running: \(isRunning)")
